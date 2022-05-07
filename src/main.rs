239707941e4df9a7df4c637ae0a68bec6ee2459e
sri <sr.ai@outlook.com>
2022-05-07 21:37:16 +0800

    Health check response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Excited to learn rust!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("Mars");
+    format!("Health Check Successful {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
