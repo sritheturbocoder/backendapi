d56ceeb5929f6000ff2020091d6192deae3968bd
sri <sr.ai@outlook.com>
2022-05-08 00:06:32 +0800

    refactor to create library

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use backendapi::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(health_check))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run()?.await
 }
