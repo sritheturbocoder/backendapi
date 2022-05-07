341cca23d6d18e9ed6c00249f8090281a97c0969
sri <sr.ai@outlook.com>
2022-05-08 00:51:03 +0800

    change port to 8080

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
+    let address = TcpListener::bind("127.0.0.1:8080")?;
     run(address)?.await
 }
